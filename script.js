2f5142e668e3654f574d359d22a0a6a3c0c65792
caiob <caioblitz@gmail.com>
2022-06-20 11:30:30 -0300

    utilizando type script com if e alert

---
+++ script.js
@@ -0,0 +1,13 @@
+(function () {
+    var _a;
+    var $ = function (query) { return document.querySelector(query); };
+    (_a = $("#cadastrar")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+        var _a, _b;
+        var nome = (_a = $("#nome")) === null || _a === void 0 ? void 0 : _a.value;
+        var placa = (_b = $("#placa")) === null || _b === void 0 ? void 0 : _b.value;
+        if (!nome || !placa) {
+            alert("Os campos nome e placa são obrigatórios");
+            return;
+        }
+    });
+})();
